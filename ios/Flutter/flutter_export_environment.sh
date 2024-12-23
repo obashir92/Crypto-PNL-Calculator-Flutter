cab1be5672a67493b8d8a639e4cf9ee8642caa7a
Osama <o.bashir92@gmail.com>
2024-12-23 18:01:24 +0100

    ...

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\dev\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\Projects\Flutter\Crypto-PNL-Calculator-Flutter"
+export "FLUTTER_APPLICATION_PATH=D:\Projects\Flutter\pnlcal"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
